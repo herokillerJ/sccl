d52f46ec3e65c6d1cf18b3e72ca89380eb7fa05c
jw <wei.jia@ipinyou.com>
2024-10-31 17:30:27 +0800

    init

---
+++ src/main/java/cn/citizenwiki/MergeAndConvert.java
@@ -0,0 +1,13 @@
+package cn.citizenwiki;
+
+/**
+ * Hello world!
+ *
+ */
+public class MergeAndConvert
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
